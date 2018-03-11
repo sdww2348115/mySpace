09cac2643127a56f8a295d9b19986955a46e9c94
sdww2348115 <2324717228@qq.com>
2018-03-11 11:34:24 +0800

    boot

---
+++ src/main/java/com/sdww8591/mySpace/MySpaceApplication.java
@@ -0,0 +1,12 @@
+package com.sdww8591.mySpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpaceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sdww8591/mySpace/MySpaceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sdww8591.mySpace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MySpaceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
